c2c2d471db3cb2fbf6e7d2d2bd28cb68a4a51c5c
Ayush <ayush421301@gmail.com>
2025-09-05 23:21:00 +0530

    fix:created pandas sepratelty for hero section design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import HeroBanner from '../assets/Hero-Banner.png';
 import Navigation from './Navigation';
+import Panda from './Panda';
 
 const Hero = () => {
     return (
@@ -13,28 +14,8 @@ const Hero = () => {
                 <div className="text-center max-w-4xl">
                     <h1 className="text-6xl md:text-8xl font-black leading-tight relative">
                         <span>BENVENUT* NELLA D</span>
-                        {/* First O replaced with panda */}
-                        <span className="inline-block w-16 h-16 md:w-20 md:h-20 relative">
-                            <div className="w-16 h-full bg-white border-2 border-black rounded-3xl relative">
-                                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                                    <div className="w-2 h-2 bg-black rounded-full"></div>
-                                    <div className="w-1 h-1 bg-black rounded-full absolute top-0 left-3"></div>
-                                    <div className="w-1 h-1 bg-black rounded-full absolute top-0 right-3"></div>
-                                    <div className="w-3 h-1 bg-black rounded-full absolute top-2 left-1/2 transform -translate-x-1/2"></div>
-                                </div>
-                            </div>
-                        </span>
-                        {/* Second O replaced with panda */}
-                        <span className="inline-block w-16 h-16 md:w-20 md:h-20 relative">
-                            <div className="w-16 h-full bg-white border-2 border-black rounded-3xl relative">
-                                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                                    <div className="w-2 h-2 bg-black rounded-full"></div>
-                                    <div className="w-1 h-1 bg-black rounded-full absolute top-0 left-3"></div>
-                                    <div className="w-1 h-1 bg-black rounded-full absolute top-0 right-3"></div>
-                                    <div className="w-3 h-1 bg-black rounded-full absolute top-2 left-1/2 transform -translate-x-1/2"></div>
-                                </div>
-                            </div>
-                        </span>
+                        <Panda />
+                        <Panda />
                         <span>DLE ART DI FRA!</span>
                     </h1>
                 </div>
